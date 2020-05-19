875e784242446e669f7b2cf88d382c50f856f805
Arjun Akkiraju <20arjuna@students.harker.org>
2020-05-18 20:28:29 -0700

    algorithm working

--- main.py
+++ main.py
@@ -13,17 +13,24 @@ def syllable_count(word):
         count += 1
     return count
 
+def calculate_score(words, sentences, syllables):
+    score = 206.835 - 1.015 * (words/sentences) - 84.6 * (syllables/words)
+    return score
 
-text = "To measure how each team performed, I'm comparing their roster, cap situation and future draft capital at the beginning of the offseason to what they have in mid-May. The most important thing a team can do is add talent, so those that made significant inroads in improving their roster will rank highly, while those that saw key pieces leave without replacements won't. I also considered how each attacked their specific needs, how well they read the market and handled the financial side of their deals, and what they did to create future draft picks. For each team, I'll include what went right, what went wrong, what they might have done differently with a bit of hindsight and what they need to do next in the months to come. Finally, and this is important: These aren't power rankings of how these teams will perform in 2020. Some of the worst teams in the league from last season will finish at or near the top of these rankings because they were able to draft immediate-impact players at key positions, while some of the best teams shed talent or weren't able to add much in the draft because they had already dealt away picks."
+text = "The color of animals is by no means a matter of chance; it depends on many considerations, but in the majority of cases tends to protect the animal from danger by rendering it less conspicuous. Perhaps it may be said that if coloring is mainly protective, there ought to be but few brightly colored animals. There are, however, not a few cases in which vivid colors are themselves protective. The kingfisher itself, though so brightly colored, is by no means easy to see. The blue harmonizes with the water, and the bird as it darts along the stream looks almost like a flash of sunlight."
 
 words = len(text.split())
 sentences = text.count(".")
 
 syllables = 0
 
-for word in text:
+for word in text.split():
     syllables+= syllable_count(word)
+
+score = calculate_score(words, sentences, syllables)
 
 print("words: " + str(words) + "\n")
 print("sentences: " + str(sentences) + "\n")
 print("syllables: " + str(syllables) + "\n")
+
+print("score: " + str(round(score, 1)))
