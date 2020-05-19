d5cd5c7064367b5c1680a98ba4cc1862a979b728
Arjun Akkiraju <20arjuna@students.harker.org>
2020-05-18 21:53:04 -0700

    adding result.html

--- flesch-readability-calculator.py
+++ flesch-readability-calculator.py
@@ -44,3 +44,8 @@ def main():
     print("syllables: " + str(syllables) + "\n")
 
     print("score: " + str(round(score, 1)))
+
+    outputFile = open("data.txt", "a")
+    outputFile.write(score)
+
+    return render_template('result.html')
