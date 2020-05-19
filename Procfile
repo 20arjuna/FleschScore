16ecbe56d9c5c3dfbbcd779d3ed4db8fd9da2983
Arjun Akkiraju <20arjuna@students.harker.org>
2020-05-18 20:41:04 -0700

    starting heroku integration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn flesch-readability-calculator:app
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from flask import Flask,render_template, Response, request, redirect, url_for, send_file
+
+
 def syllable_count(word):
     word = word.lower()
     count = 0
@@ -17,20 +20,24 @@ def calculate_score(words, sentences, syllables):
     score = 206.835 - 1.015 * (words/sentences) - 84.6 * (syllables/words)
     return score
 
-text = "The color of animals is by no means a matter of chance; it depends on many considerations, but in the majority of cases tends to protect the animal from danger by rendering it less conspicuous. Perhaps it may be said that if coloring is mainly protective, there ought to be but few brightly colored animals. There are, however, not a few cases in which vivid colors are themselves protective. The kingfisher itself, though so brightly colored, is by no means easy to see. The blue harmonizes with the water, and the bird as it darts along the stream looks almost like a flash of sunlight."
+@app.route('/')
+def hello():
+    return render_template('frontend.html')
 
-words = len(text.split())
-sentences = text.count(".")
+@app.route('/mainlocal', methods=['POST'])
+def main():
+    words = len(text.split())
+    sentences = text.count(".")
 
-syllables = 0
+    syllables = 0
 
-for word in text.split():
-    syllables+= syllable_count(word)
+    for word in text.split():
+        syllables+= syllable_count(word)
 
-score = calculate_score(words, sentences, syllables)
+    score = calculate_score(words, sentences, syllables)
 
-print("words: " + str(words) + "\n")
-print("sentences: " + str(sentences) + "\n")
-print("syllables: " + str(syllables) + "\n")
+    print("words: " + str(words) + "\n")
+    print("sentences: " + str(sentences) + "\n")
+    print("syllables: " + str(syllables) + "\n")
 
-print("score: " + str(round(score, 1)))
+    print("score: " + str(round(score, 1)))
