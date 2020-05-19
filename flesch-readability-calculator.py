ca3227cbc294452e034e467a54838ff0b151bc57
Arjun Akkiraju <20arjuna@students.harker.org>
2020-05-18 22:34:40 -0700

    adding analysis

--- flesch-readability-calculator.py
+++ flesch-readability-calculator.py
@@ -21,6 +21,23 @@ def calculate_score(words, sentences, syllables):
     score = 206.835 - 1.015 * (words/sentences) - 84.6 * (syllables/words)
     return score
 
+def getAnalysis(score):
+    if (score <= 30):
+        return "Very difficult to read. Professional reading level."
+    elif (score <= 50):
+        return "Difficult to read. College reading level"
+    elif (score <= 60):
+        return "Fairly difficult to read. 10th - 12th grade level."
+    elif (score <= 70):
+        return "Plain English. 8th - 9th grade level."
+    elif (score <= 80):
+        return "Fairly easy to read. 7th grade reading level."
+    elif (score <= 90):
+        return "Easy to read. 6th grade reading level."
+    elif (score <= 100):
+        return "Very easy to read. Kids stuff."
+
+
 @app.route('/')
 def hello():
     return render_template('frontend.html')
@@ -39,11 +56,8 @@ def main():
 
     score = calculate_score(words, sentences, syllables)
 
-    print("words: " + str(words) + "\n")
-    print("sentences: " + str(sentences) + "\n")
-    print("syllables: " + str(syllables) + "\n")
-
-    print("score: " + str(round(score, 1)))
+    analysis = getAnalysis(score)
 
 
-    return  '<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script><style>body {background-color: black;} h1{color: white;display: flex;flex-direction: column;justify-content: center;text-align: center;}img {display: block; margin-left: auto;margin-right: auto;}</style><h1>' + str(round(score, 1)) + '</h1>'
+
+    return  '<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script><style>body {background-color: black;} h1{color: white;display: flex;flex-direction: column;justify-content: center;text-align: center;}img {display: block; margin-left: auto;margin-right: auto;}h4{color: white;display: flex;flex-direction: column;justify-content: center;text-align: center;}</style><h1>' + str(round(score, 1)) + '</h1>' + '<h4>' + analysis + '</h4>'
