664f1f3ba083b173a43405d1d1701d6dcf721500
Arjun Akkiraju <20arjuna@students.harker.org>
2020-07-31 11:32:19 -0700

    test

--- flesch-readability-calculator.py
+++ flesch-readability-calculator.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template, Response, request, redirect, url_for, send_file
 
 app = Flask(__name__)
-
+print("eh")
 def syllable_count(word):
     word = word.lower()
     count = 0
